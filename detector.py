2fc714f17b785eff02e50a18d6a347629699fccb
sonya <kanukova.s.a@gmail.com>
2019-02-20 17:45:55 +0300

    detection improved

--- detector.py
+++ detector.py
@@ -1,42 +1,64 @@
 import cv2
 import numpy as np
+import os
 
-original_img = cv2.imread('/home/sonya/zoomos/images/577986.jpg', 0)
-template = cv2.imread('template.jpg', 0)
+color = cv2.COLOR_BGR2GRAY
 
-kernel_sharpening = np.array([[-1,-1,-1], 
-                              [-1, 9,-1],
-                              [-1,-1,-1]])
+kernel_sharpening = np.array([[-1,-1,-1], [-1, 9,-1], [-1,-1,-1]])
 
-sharp_img = cv2.filter2D(original_img, -1, kernel_sharpening)
-sharp_template = cv2.filter2D(template, -1, kernel_sharpening)
+images_dir = "/home/sonya/zoomos/watermark-removal/images/";
 
-w, h = template.shape[::-1]
+results_dir = images_dir + "results/"
+items_dir = images_dir + "items/"
+templates_dir = images_dir + "templates/"
 
-methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
-            'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
+for item_filename in os.listdir(items_dir):
 
-mask = np.zeros(sharp_img.shape, sharp_img.dtype)
+	original_img = cv2.imread(items_dir + item_filename)
+	img_color = cv2.cvtColor(original_img, color)
+	sharp_img = cv2.filter2D(img_color, -1, kernel_sharpening)
 
-for meth in methods:
-    method = eval(meth)
+	saved = False
 
-    res = cv2.matchTemplate(sharp_img, sharp_template, method)
-    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+	for template_filename in os.listdir(templates_dir):
 
-    if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
-        top_left = min_loc
-    else:
-        top_left = max_loc
-    bottom_right = (top_left[0] + w, top_left[1] + h)
+		if saved:
+			break
 
-    cv2.rectangle(original_img, top_left, bottom_right, (255, 0, 255), cv2.FILLED)
+		template = cv2.imread(templates_dir + template_filename)
+		template_color = cv2.cvtColor(template, color)
 
-# result_img = cv2.inpaint(original_img, mask, 10, cv2.INPAINT_NS)
+		for i in np.arange(2.0, 0.5, -0.03):
+			resized_template = cv2.resize(template_color, (0,0), fx=i, fy=i)
+			sharp_template = cv2.filter2D(resized_template, -1, kernel_sharpening)
 
-cv2.imshow('original', original_img)
-# cv2.imshow('mask', mask)
-# cv2.imshow('result', result_img)
+			w, h = sharp_template.shape[::-1]
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+			method = eval('cv2.TM_CCOEFF_NORMED')
+
+			if w > sharp_img.shape[1] or h > sharp_img.shape[0]:
+				continue
+
+			res = cv2.matchTemplate(sharp_img, sharp_template, method)
+
+			min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+
+			if max_val < 0.4:
+				continue
+
+			top_left = max_loc
+
+			if top_left[1] == 0 or top_left[1] == sharp_img.shape[0]:
+				continue
+
+			bottom_right = (top_left[0] + w, top_left[1] + h)
+
+			cv2.rectangle(original_img, top_left, bottom_right, (255, 0, 255), cv2.FILLED)
+
+			cv2.imwrite(results_dir + item_filename, original_img)
+
+			saved = True
+			break
+
+	if not saved:
+		cv2.imwrite(results_dir + "bad/" + item_filename, original_img)
